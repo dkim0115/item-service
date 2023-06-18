6c14cc6ebc50e102f83d0c9df05c98089a56eb9c
juheeson <bookck13@gmail.com>
2023-06-18 13:13:48 -0400

    second commit

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -1,21 +1,21 @@
 package hello.itemservice.domain.item;
 
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 @Data
-public class item {
+public class Item {
 
     private Long id;
     private String itemName;
     private Integer price;
     private Integer quantity;
 
-    public item() {
+    public Item() {
     }
 
-    public item(String itemName, Integer price, Integer quantity) {
+    public Item(String itemName, Integer price, Integer quantity) {
         this.itemName = itemName;
         this.price = price;
         this.quantity = quantity;
