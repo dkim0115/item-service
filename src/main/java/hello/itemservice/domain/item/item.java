b8a29fc89bfc16a7ce048b852c873bb55c858c22
bookck13 <bookck13@gmail.com>
2023-06-18 01:04:47 -0400

    first commit

---
+++ src/main/java/hello/itemservice/domain/item/item.java
@@ -0,0 +1,23 @@
+package hello.itemservice.domain.item;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+public class item {
+
+    private Long id;
+    private String itemName;
+    private Integer price;
+    private Integer quantity;
+
+    public item() {
+    }
+
+    public item(String itemName, Integer price, Integer quantity) {
+        this.itemName = itemName;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
---
+++ src/main/java/hello/itemservice/domain/item/itemRepository.java
@@ -0,0 +1,10 @@
+package hello.itemservice.domain.item;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Repository
+public class itemRepository {
+
+
+}
